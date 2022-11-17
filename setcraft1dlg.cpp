3aaeccead3d5e9b65c9ad1bd14d892071cb645a0
qubo188612 <346386694@qq.com>
2022-11-17 10:47:07 +0800

    first commit

--- setcraft1dlg.cpp
+++ setcraft1dlg.cpp
@@ -23,3 +23,51 @@ void setcraft1Dlg::close_dlg_show()
 {
 
 }
+
+void setcraft1Dlg::on_pushButton_get_stpos_clicked()//获取起点坐标姿态
+{
+
+}
+
+
+void setcraft1Dlg::on_pushButton_updata_stpos_clicked()//更新起点坐标姿态
+{
+
+}
+
+
+void setcraft1Dlg::on_pushButton_get_edpos_clicked()//获取终点坐标姿态
+{
+
+}
+
+
+void setcraft1Dlg::on_pushButton_updata_edpos_clicked()//更新终点坐标姿态
+{
+
+}
+
+
+void setcraft1Dlg::on_pushButton_get_centerpos_clicked()//获取中间点坐标姿态
+{
+
+}
+
+
+void setcraft1Dlg::on_pushButton_updata_centerpos_clicked()//插入中间点坐标姿态
+{
+
+}
+
+
+void setcraft1Dlg::on_pushButton_del_clicked()//删除选中姿态
+{
+
+}
+
+
+void setcraft1Dlg::on_pushButtonOK_clicked()//确定并保存
+{
+
+}
+
--- setcraft1dlg.h
+++ setcraft1dlg.h
@@ -20,6 +20,23 @@ public:
 
     void init_dlg_show();
     void close_dlg_show();
+private slots:
+    void on_pushButton_get_stpos_clicked();
+
+    void on_pushButton_updata_stpos_clicked();
+
+    void on_pushButton_get_edpos_clicked();
+
+    void on_pushButton_updata_edpos_clicked();
+
+    void on_pushButton_get_centerpos_clicked();
+
+    void on_pushButton_updata_centerpos_clicked();
+
+    void on_pushButton_del_clicked();
+
+    void on_pushButtonOK_clicked();
+
 private:
     Ui::setcraft1Dlg *ui;
 };
