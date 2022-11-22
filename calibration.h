f069efff7c5880521e3584da398e6fe3d43b3256
qubo188612 <346386694@qq.com>
2022-11-22 17:07:05 +0800

    first commit

--- calibration.h
+++ calibration.h
@@ -11,7 +11,7 @@
 
 typedef enum CAL_POSTURE_MODEL_ID          //机器人姿态角含义模式
 {
-    CAL_ROBOT_YASKAWA=0,        //安川、ABB阿姿态标准 内旋 RX-RY-RZ
+    CAL_ROBOT_YASKAWA=0,        //安川、ABB姿态标准 内旋 RX-RY-RZ
     CAL_ROBOT_KUKA=1,           //库卡姿态标准 外旋 RZ-RY-RX
     CAL_ROBOT_PANASONIC=2,      //松下姿态标准 内旋 RX-"RY"-RZ
 }CAL_POSTURE;
