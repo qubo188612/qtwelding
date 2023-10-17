019a92a2ed728f158aedaa5930129c12b0b8b665
qubo188612 <346386694@qq.com>
2023-10-17 10:33:04 +0800

    first commit

--- XTcp.cpp
+++ XTcp.cpp
@@ -91,15 +91,9 @@ bool XTcp::Listen(unsigned short num) //监听端口号
 bool XTcp::SetRcvBufferlong(int bufferlong) //设置接收缓冲长度
 {
     int sendBufLen = bufferlong;
-    int tmp;
-    socklen_t optlen = sizeof(int);
-    setsockopt(tsock,SOL_SOCKET, SO_RCVBUF,(const char*)&sendBufLen, sizeof(int));
-#if _MSC_VER
-    getsockopt(tsock,SOL_SOCKET, SO_RCVBUF,(char *)&tmp, &optlen);
-#else
-    getsockopt(tsock,SOL_SOCKET, SO_RCVBUF,(int *)&tmp, &optlen);
-#endif
-    if(tmp<sendBufLen)
+    int rc;
+    rc=setsockopt(tsock,SOL_SOCKET, SO_RCVBUF,(const char*)&sendBufLen, sizeof(int));
+    if(rc!=0)
     {
         return true;
     }
@@ -112,15 +106,9 @@ bool XTcp::SetRcvBufferlong(int bufferlong) //设置接收缓冲长度
 bool XTcp::SetSentBufferlong(int bufferlong) //设置发送缓冲长度
 {
     int sendBufLen = bufferlong;
-    int tmp;
-    socklen_t optlen = sizeof(int);
-    setsockopt(tsock,SOL_SOCKET, SO_SNDBUF,(const char*)&sendBufLen, sizeof(int));
-#if _MSC_VER
-    getsockopt(tsock,SOL_SOCKET, SO_SNDBUF,(char *)&tmp, &optlen);
-#else
-    getsockopt(tsock,SOL_SOCKET, SO_SNDBUF,(int *)&tmp, &optlen);
-#endif
-    if(tmp<sendBufLen)
+    int rc;
+    rc=setsockopt(tsock,SOL_SOCKET, SO_SNDBUF,(const char*)&sendBufLen, sizeof(int));
+    if(rc!=0)
     {
         return true;
     }
